1be5b6f54029410e45b9a25476a174bc7bf9feb0
simran1901 <simranmakhijani55@gmail.com>
2020-11-27 00:48:09 +0530

    working with providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:my_shop/screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
+import './providers/products.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,14 +12,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
-      ),
-      home: ProductOverviewScreen(),
+    return ChangeNotifierProvider(
+      builder: (ctx) => Products(),
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          }),
     );
   }
 }
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,65 +1,16 @@
 import 'package:flutter/material.dart';
 
-import '../models/product.dart';
-import '../widgets/product_item.dart';
+//import '../models/product.dart';
+import '../widgets/products_grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
-  final List<Product> loadedProducts = [
-    Product(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Blue Dress',
-      description: 'A blue dress - it is pretty blue!',
-      price: 50.99,
-      imageUrl:
-          'https://img.promgirl.com/_img/PGPRODUCTS/1727401/1000/peacock-dress-JO-JVN-JVN21899p-b.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Nike Shoes',
-      description: 'These are quite - Nike strong',
-      price: 20.50,
-      imageUrl:
-          'https://www.prodirectbasketball.com/productimages/V3_1_Main/210519_Main_Thumb_0575959.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'Hair dryer',
-      description: 'A hair dryer - dries your hair well.',
-      price: 30.90,
-      imageUrl:
-          'https://www.the-best-hair-dryer-reviews.com/wp-content/uploads/2013/10/remington-hair-dryer-best-hair-dryer-reviews5.jpg',
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10.0),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(
-          loadedProducts[i].id,
-          loadedProducts[i].title,
-          loadedProducts[i].imageUrl,
-        ),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductsGrid(),
     );
   }
 }

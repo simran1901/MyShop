8767c9d2dd3bb4acbaa6c18498f09ceeb9f07ae5
simran1901 <simranmakhijani55@gmail.com>
2020-12-02 03:01:34 +0530

    working on expanded icon

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -32,7 +32,7 @@ class CartScreen extends StatelessWidget {
                   Spacer(),
                   Chip(
                     label: Text(
-                      'Rs. ${cart.totalAmount}',
+                      'Rs. ${cart.totalAmount.toStringAsFixed(2)}',
                       style: TextStyle(
                         color:
                             Theme.of(context).primaryTextTheme.headline6.color,

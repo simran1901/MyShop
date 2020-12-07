1c3f1cde6ebc8a59434e0137fb4ee5dc12957ace
simran1901 <simranmakhijani55@gmail.com>
2020-12-08 03:41:20 +0530

    added snackbar

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -37,6 +37,18 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text(
+              'Do you want to remove the item from the cart?',
+            ),
+            actions: [],
+          ),
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },

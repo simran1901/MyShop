5ac115e8aa83655691c6c49dec4ddf4e9bae75d8
simran1901 <simranmakhijani55@gmail.com>
2020-12-14 04:34:55 +0530

    Handling errors

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -67,10 +67,10 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct(Product product) {
+  Future<void> addProduct(Product product) {
     const url =
         'https://simran-s-shop-default-rtdb.firebaseio.com/products.json';
-    http
+    return http
         .post(
       url,
       body: json.encode({
@@ -91,6 +91,9 @@ class Products with ChangeNotifier {
       );
       _items.add(newProduct);
       notifyListeners();
+    }).catchError((error) {
+      print(error);
+      throw error;
     });
   }
 

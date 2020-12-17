5596d3d08d9848f96680aa2d4d894aa2b6a6ca4d
simran1901 <simranmakhijani55@gmail.com>
2020-12-17 17:28:29 +0530

    fetching data

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -67,6 +67,17 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
+  Future<void> fetchAndSetProducts() async {
+    const url =
+        'https://simran-s-shop-default-rtdb.firebaseio.com/products.json';
+    try {
+      final response = await http.get(url);
+      print(json.decode(response.body));
+    } catch (error) {
+      throw (error);
+    }
+  }
+
   Future<void> addProduct(Product product) async {
     const url =
         'https://simran-s-shop-default-rtdb.firebaseio.com/products.json';

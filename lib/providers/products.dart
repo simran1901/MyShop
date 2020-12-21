39c06d522017446e446bd1c044d23b07bb8bec43
simran1901 <simranmakhijani55@gmail.com>
2020-12-22 03:21:29 +0530

    attaching products to users

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -72,9 +72,10 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  Future<void> fetchAndSetProducts() async {
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    final filterString = filterByUser ? 'orderBy="creatorId"&equalTo="$userId"' : '';
     var url =
-        'https://simran-s-shop-default-rtdb.firebaseio.com/products.json?auth=$authToken';
+        'https://simran-s-shop-default-rtdb.firebaseio.com/products.json?auth=$authToken&$filterString';
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -116,6 +117,7 @@ class Products with ChangeNotifier {
           'description': product.description,
           'imageUrl': product.imageUrl,
           'price': product.price,
+          'creatorId': userId,
           // 'isFavorite': product.isFavorite,
         }),
       );
